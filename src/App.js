54c897dfa76ce5632e1099a4eb667318e6596ef6
mutai-gilbert <mutaigilbert85@gmail.com>
2024-01-12 17:29:31 +0300

    Add new changes to the App.js file file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import React, { useState } from 'react';
 import "./App.css";
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(
-  https://mkxypgtnygxenagtejko.supabase.co,
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1reHlwZ3RueWd4ZW5hZ3RlamtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM0MDIwNjMsImV4cCI6MjAxODk3ODA2M30.knML223u4JdXA3Lql5B7mrR4esYrvgi01Sl2Yap8Zug,
-);
+const supabaseUrl = 'https://mkxypgtnygxenagtejko.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1reHlwZ3RueWd4ZW5hZ3RlamtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM0MDIwNjMsImV4cCI6MjAxODk3ODA2M30.knML223u4JdXA3Lql5B7mrR4esYrvgi01Sl2Yap8Zug'
+const supabase = createClient(supabaseUrl, SUPABASE_KEY);
+
 function App() {
   // storing data in state
   const [inputData, setInputData] = useState(null);
@@ -13,7 +14,7 @@ function App() {
 
   //making call to edge function to create embeddings
   const handleEmbed = async () => {
-    const { data, error } = await supabase.functions.invoke(
+    const { error } = await supabase.functions.invoke(
       "create-embeddings",
       {
         body: { input: inputData },
