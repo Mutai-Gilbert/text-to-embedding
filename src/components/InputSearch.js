bf01d5e824b9313826f14d1f07551e417319f2d7
mutai-gilbert <mutaigilbert85@gmail.com>
2024-01-14 19:40:14 +0300

    Add new changes

---
+++ src/components/InputSearch.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+
+const Input = () => {
+    return (
+        <>
+        <div className="mb-3 container d-flex align-items-center justify-content-center mt-5" style={{ maxWidth: "22rem"}}>
+        <InputGroup className="mb-3">
+        <Form.Control
+        placeholder="Recipient's username"
+        aria-label="Recipient's username"
+        aria-describedby="basic-addon2"
+        />
+        <Button variant="outline-secondary" id="button-addon2">
+        Button
+        </Button>
+        </InputGroup>
+        </div>
+        </>
+    )
+};
+
+export default Input;
