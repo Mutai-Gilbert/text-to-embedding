2ec9b6001a1132dbc239cb1a250e01f37f97857c
mutai-gilbert <mutaigilbert85@gmail.com>
2024-01-16 22:52:14 +0300

    Add new changes

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -6,38 +6,38 @@ import InputGroup from 'react-bootstrap/InputGroup';
 
 const Input = () => {
     const [text, setText] = useState("");
-    const submitHandler = (e) => {
-        e.preventDefault();
+   const submitHandler = (e) => {
+    e.preventDefault();
 
-        const payload = { input: text }
+    const payload = { input: text }
 
-        const headers = {
-            Authorization: `Bearer ${import.meta.env.REACT_APP_KEY}`,
-            "Content-Type": "application/json",
-        }
-        axios.post("http://127.0.0.1:54321/functions/v1/textembed", payload, { headers,})};
+    const headers = {
+        Authorization: `Bearer ${process.env.REACT_APP_KEY}`,
+        "Content-Type": "application/json",
+    }
+    axios.post("http://127.0.0.1:54321/functions/v1/textembed", payload, { headers });
+};
 
-    return (
-        <>
+return (
+    <>
         <div className="mb-3 container d-flex align-items-center justify-content-center mt-5" style={{ maxWidth: "22rem"}}>
-        <Form onSubmit={submitHandler}>
-
-            <InputGroup className="mb-3" onChange={(e) => setText(e.target.value)} defaultValue={text} >
-                <Form.Control
-                    placeholder="Recipient's username"
-                    aria-label="Recipient's username"
-                    aria-describedby="basic-addon2"
-                />
-            
-                <Button type="submit" variant="outline-secondary" id="button-addon2">
-                    Button
-                </Button>
-            </InputGroup>
-            
-        </Form>
+            <Form onSubmit={submitHandler}>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                        placeholder="Recipient's username"
+                        aria-label="Recipient's username"
+                        aria-describedby="basic-addon2"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                    />
+                    <Button type="submit" variant="outline-secondary" id="button-addon2">
+                        Submit
+                    </Button>
+                </InputGroup>
+            </Form>
         </div>
-        </>
-    )
+    </>
+) 
 };
 
 export default Input;
