ba870a08b70bce8af684eb4213e14b7c11fc4888
mutai-gilbert <mutaigilbert85@gmail.com>
2024-01-16 14:45:45 +0300

    Add new changes

--- src/components/InputSearch.js
+++ src/components/InputSearch.js
@@ -4,23 +4,37 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-
 const Input = () => {
     const [text, setText] = useState("");
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const payload = { input: text }
+
+        const headers = {
+            Authorization: `Bearer ${import.meta.env.REACT_APP_KEY}`,
+            "Content-Type": "application/json",
+        }
+        axios.post("http://127.0.0.1:54321/functions/v1/textembed", payload, { headers,})};
 
     return (
         <>
         <div className="mb-3 container d-flex align-items-center justify-content-center mt-5" style={{ maxWidth: "22rem"}}>
-        <InputGroup className="mb-3">
-        <Form.Control
-        placeholder="Recipient's username"
-        aria-label="Recipient's username"
-        aria-describedby="basic-addon2"
-        />
-        <Button variant="outline-secondary" id="button-addon2">
-        Button
-        </Button>
-        </InputGroup>
+        <Form onSubmit={submitHandler}>
+
+            <InputGroup className="mb-3" onChange={(e) => setText(e.target.value)} defaultValue={text} >
+                <Form.Control
+                    placeholder="Recipient's username"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                />
+            
+                <Button type="submit" variant="outline-secondary" id="button-addon2">
+                    Button
+                </Button>
+            </InputGroup>
+            
+        </Form>
         </div>
         </>
     )
